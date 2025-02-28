20c97fe6771acdfb36ae66df09b3151897772828
Straitspartners <projectcare@straitspartners.co>
2025-02-28 15:20:38 +0530

    Update SignUp.js

--- Frontend/src/SignUp.js
+++ Frontend/src/SignUp.js
@@ -25,7 +25,11 @@ const Signup = () => {
     if (formRef.current.checkValidity()) {
       const userData = { mobile_number, username, password, dob, gender, password_confirmation, email };
 
-      axios.post('https://happy-dune-02aab6b00.6.azurestaticapps.net/register/', userData)
+      axios.post('https://happy-dune-02aab6b00.6.azurestaticapps.net/register/', userData, {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
         .then(response => {
           setMessage('Registration successful. Please verify your OTP.');
           setError('');
