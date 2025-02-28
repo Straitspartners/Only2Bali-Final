c425d13d3834f1f61012646623701f0e3096aa82
Straitspartners <projectcare@straitspartners.co>
2025-02-28 12:49:55 +0530

    Update ItineraryPage.js

--- Frontend/src/ItineraryPage.js
+++ Frontend/src/ItineraryPage.js
@@ -789,9 +789,9 @@ const ItineraryPage = () => {
             <tr>
               <th class="footer-table-tr1">Only 2 Bali</th>
               <th class="footer-table-tr2">
-                <img src={facebook} alt={fb}  className="social-icon"/>
-                <img src={instagram} alt={fb}  className="social-icon" />
-                <img src={linkedin} alt={fb} className="social-icon"/>
+                <img src={facebook} alt={}  className="social-icon"/>
+                <img src={instagram} alt={}  className="social-icon" />
+                <img src={linkedin} alt={} className="social-icon"/>
               </th>
             </tr>
           </thead>
