73517c9da2ef04b5716059fc101eea831f932732
Straitspartners <projectcare@straitspartners.co>
2025-02-28 23:40:31 +0530

    Update wsgi.py

--- Backend/only2bali/wsgi.py
+++ Backend/only2bali/wsgi.py
@@ -19,7 +19,7 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-settings_module = 'crud.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'only2bali.settings'
+settings_module = 'only2bali.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'only2bali.settings'
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_wsgi_application()
